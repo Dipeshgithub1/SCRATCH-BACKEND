1c9b8cbf2322ed5d91e6a129b726b8d7930926b0
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-12-19 23:16:17 +0530

    register routes

--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,25 +1,35 @@
 const express = require('express')
 const router = express.Router()
 const userModel = require("../models/usermodel")
+const bcrypt = require("bcrypt")
 
 
 router.get("/",(req,res) => {
     res.send("hello this admin")
 })
 
-router.post("/register",async(req,res) => {
+router.post("/register",(req,res) => {
     try {
-    let {fullname,email,password} = req.body;
-    let user = await userModel.create({
-        fullname,
-        email,
-        password
+    let {email,password,fullname} = req.body;
+
+    bcrypt.genSalt(10,function(err,salt){
+    bcrypt.hash(password,salt,async function(err, hash){
+        if(err) return res.send(err.message);
+        else{
+            let user = await userModel.create({
+                email,
+                password:hash,
+                fullname
+            });
+         let token =   jwt.sign({email,id:user._id},"heyhey")
+         res.cookie("token",token);
+         res.send("user created successfully")
+        }
+    })    
     })
-    res.send(user)
-        
+       
     } catch (error) {
-        console.log(error)
-        
+        res.send(error.message)    
     }
     
 })

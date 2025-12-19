1105a87720748bd8a73769962e27031f3bd7906a
Dipesh Kumar <dipesh.kumar@iic.ac.in>
2025-12-19 19:58:10 +0530

    owners routes

--- models/usermodel.js
+++ models/usermodel.js
@@ -27,18 +27,15 @@ password: {
 },
 
 contact: {
-    type: Number,
-     required:true
+    type: Number
 },
 
 dob: {
-    type: Date,
-     required:true
+    type: Date
 },
 
 picture: {
     type: String,
-     required:true,
      trim:true
 },
 
--- routes/UsersRouter.js
+++ routes/UsersRouter.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const userModel = require("../models/usermodel")
 
-router.get("/admin",(req,res) => {
+
+router.get("/",(req,res) => {
     res.send("hello this admin")
+})
+
+router.post("/register",async(req,res) => {
+    try {
+    let {fullname,email,password} = req.body;
+    let user = await userModel.create({
+        fullname,
+        email,
+        password
+    })
+    res.send(user)
+        
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
 })
 
 module.exports = router;
